37595793faddd63d833265fd864e648d29b6a46a
SpaceWaffles <jackhartwig7@gmail.com>
2024-05-07 17:55:04 -0500

    set port of pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,14 @@
 module.exports = {
     apps: [
         {
-            name: 'Not-Feastables-Front',
+            name: 'Nova-Front',
             script: 'npm',
             args: 'run build && npm start',
             exec_mode: 'fork',
-            instances: 1
+            instances: 1,
+            env: {
+                PORT: 5000 // Setting environment variable PORT to 5000
+            }
         },
     ],
 };
