35b6c74aa2c228d305a914a0ea57b4b055b267c0
SpaceWaffles5827 <jackhartwig7@gmail.com>
2024-05-07 23:48:54 +0000

    final pm2 fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,7 @@ module.exports = {
         {
             name: 'Not-Feastables-Front',
             script: 'npm',
-            args: 'run build && npm start',
-            exec_mode: 'fork',
-            instances: 1,
+            args: 'run start',
             env: {
                 PORT: 5000 // Setting environment variable PORT to 5000
             }
